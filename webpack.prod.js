55fc29e7339ee3eae1f2e457ec96f413d8c49679
Ananda Sheva Hidayat <sheva1257@gmail.com>
2025-06-13 08:42:13 +0700

    Configure for Github Pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    // Tambahkan publicPath untuk GitHub Pages
+    publicPath: process.env.NODE_ENV === 'production' ? '/storymap-app/' : '/',
+  },
   module: {
     rules: [
       {
